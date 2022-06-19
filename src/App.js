93c07f69e7403aef8444d58032c66d859a2e5849
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-06-18 23:55:05 -0300

    Api digimon com react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [digimon, setDigimon] = useState([]);
+
+  fetch("https://digimon-api.vercel.app/api/digimon", {
+    method: "GET",
+    headers: {
+      "content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setDigimon(data);
+    });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex-wrap d-flex gap-3  justify-content-center mt-4">
+
+
+
+
+      {digimon.map((data) => {
+        return (
+
+          <div className="card" style={{ width: "300px" }}>
+
+            <img src={data.img} class="card-img-top" />
+
+            <div className="card-body ">
+
+              <h5 class="card-title bg-primary text-center text-white">{data.level}</h5>
+
+              <p className="card-text bg-success  text-white text-center" >{data.name} </p>
+
+            </div>
+
+          </div>
+
+
+        );
+      })}
+
+
+
+
+
+
+
     </div>
   );
 }
